7aa39f175d91a5a8688f7bfe42c365629cb82af2
Amber Mohler <ambermohler@iMac.local>
2020-03-24 13:23:40 -0400

    added sessionConfig object to server.js and got login endpoint working

--- api/server.js
+++ api/server.js
@@ -1,14 +1,29 @@
 const express = require("express");
 
+const session = require("express-session");
+
 const usersRouter = require("../users/users-router");
 const authRouter = require("../auth/auth-router");
 
 const server = express();
 
+const sessionConfig = {
+  name: "monster",
+  secret: "super secret secret",
+  cookie: {
+    maxAge: 1000 * 60 * 60,
+    secure: false,
+    httpOnly: true
+  },
+  resave: false,
+  saveUninitialized: true
+};
+
 server.use(express.json());
+server.use(session(sessionConfig));
 
 server.use("/api/users", usersRouter);
-server.use("api/auth", authRouter);
+server.use("/api/auth", authRouter);
 
 server.get("/", (req, res) => {
   res.json({ api: "running" });
